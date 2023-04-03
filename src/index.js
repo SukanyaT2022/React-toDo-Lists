ed4114c4e1e24fb47793cb448f1fb08fe8cddd90
TiparpronS <Sukanyat2022a@gmail.com>
2023-04-03 09:20:27 -0400

    add

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import JavaScriptPractice from './JavaScriptPractice';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <JavaScriptPractice/>
+    <App />
+    {/* <JavaScriptPractice/> */}
   </React.StrictMode>
 );
 
